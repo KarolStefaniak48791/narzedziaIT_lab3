86f3b309f5d146fbad4e4462615d21308910b94d
KarolStefaniakk <dsw48791@student.dsw.edu.pl>
2023-04-19 16:36:28 +0200

    lab3

---
+++ learngitbranching_komendy.sh
@@ -0,0 +1,50 @@
+git commit
+git commit
+
+git checkout -b bugFix
+
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+
+git checkout C4
+
+git checkout bugFix^
+
+git branch -f main C6
+git checkout HEAD~1
+git branch -f bugFix HEAD~1
+
+git reset HEAD~1
+git checkout pushed
+git revert HEAD
+
+git cherry-pick C3 C4 C7
+
+git rebase -i main
+git rebase bugFix main
+
+git rebase -i HEAD~2
+git commit --amend
+git rebase -i HEAD~2
+git rebase caption main
+
+git checkout main
+git cherry-pick C2
+git commit --amend
+git cherry-pick C3
+
+git tag v1 side~1
+git tag v0 main~2
+git checkout v1
+
+git commit
